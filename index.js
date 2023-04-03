1370ac33efde929f7d7d3f0cc54402bf71472dea
Will <willchesson@gmail.com>
2023-04-03 17:29:50 -0400

    click event working

--- index.js
+++ index.js
@@ -1,28 +1,45 @@
 let authorArray;
-
+const authorBar = document.querySelector("#author-display");
+const imageDetail = document.querySelector(".image-detail");
+const authorDetail = document.querySelector("#author-details")
 
 document.addEventListener("DOMContentLoaded", () => {
-    const authorBar = document.querySelector("#author-display")
-    getAuthors();
+  getAuthors();
 
-    function getAuthors() {
-        fetch("http://localhost:3000/authors")
-        .then(r => r.json())
-        .then(data => {
-            console.log(data);
-            authorArray = data;
-            renderAuthorImages(authorArray)
-        })
-    }
+  function getAuthors() {
+    fetch("http://localhost:3000/authors")
+      .then((r) => r.json())
+      .then((data) => {
+        console.log(data);
+        authorArray = data;
+        renderAuthorImages(authorArray);
+      });
+  }
 
-    function renderAuthorImages(authorArray) {
-        authorBar.innerHTML = authorArray.map(renderEachImage).join("")
-    }
+  function renderAuthorImages(authorArray) {
+    authorBar.innerHTML = authorArray.map(renderEachImage).join("");
+  }
 
-    function renderEachImage(author) {
-        return `
+  function renderEachImage(author) {
+    return `
         <img id=${author.id} class="author-image" src="${author.image}" />
-        `
-    }
+        `;
+  }
+});
+//DOMContentLoaded ends
 
-})
+document.addEventListener("click", (e) => {
+  if (e.target.className === "author-image") {
+    const authorToDisplay = authorArray.find(
+      (author) => author.id === parseInt(e.target.id)
+    );
+    renderAuthorDetails(authorToDisplay);
+  }
+});
+
+function renderAuthorDetails(author) {
+  authorDetail.innerHTML = `
+  <img class="image-detail" src="${author.image}">
+  <h3 class="author-name">${author.name}</h3>
+  `
+}
