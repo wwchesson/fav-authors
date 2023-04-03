16b0c84c18c34cc1136bc32853be67ac2c8465f1
Will <willchesson@gmail.com>
2023-04-03 18:09:42 -0400

    iframe

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let authorArray;
 const authorBar = document.querySelector("#author-display");
 const imageDetail = document.querySelector(".image-detail");
-const authorDetail = document.querySelector("#author-details")
+const authorDetail = document.querySelector("#author-details");
+const videoPlayer = () => document.querySelector("#player");
 
 document.addEventListener("DOMContentLoaded", () => {
   getAuthors();
@@ -33,13 +34,52 @@ document.addEventListener("click", (e) => {
     const authorToDisplay = authorArray.find(
       (author) => author.id === parseInt(e.target.id)
     );
-    renderAuthorDetails(authorToDisplay);
+    const authorBooks = authorToDisplay.books;
+
+    renderAuthorDetails(authorToDisplay, authorBooks);
   }
 });
 
-function renderAuthorDetails(author) {
+function renderAuthorDetails(author, books) {
   authorDetail.innerHTML = `
   <img class="image-detail" src="${author.image}">
-  <h3 class="author-name">${author.name}</h3>
-  `
+  <h2 class="author-name">${author.name}</h2>
+  <h3> Books: </h3>
+  ${books.map(renderBooks).join("")}
+  <button class="video-button" id=${author.id}>See Lecture</button>
+  `;
+}
+
+function renderBooks(book) {
+  return `
+    <h4 class="books">${book}</h4>
+    `;
+}
+
+document.addEventListener("click", (e) => {
+  if (e.target.className === "video-button") {
+    const authorVideo = authorArray.find(
+      (author) => author.id === parseInt(e.target.id)
+    );
+    iFrame(authorVideo);
+  }
+});
+
+function iFrame(author) {
+  authorDetail.innerHTML = `
+  <iframe id="player" class="inset-0 w-full h-full" frameborder="0" ></iframe>    `;
+  videoPlayer().src = `https://www.youtube.com/embed/${extractVideoID(
+    author.video
+  )}`;
+}
+
+function extractVideoID(url) {
+  var regExp =
+    /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+  var match = url.match(regExp);
+  if (match && match[7].length == 11) {
+    return match[7];
+  } else {
+    alert("Could not extract video ID.");
+  }
 }
